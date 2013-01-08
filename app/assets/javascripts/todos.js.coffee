e8007f009adf0fccd41d1e2b703aede652d61fd2
Diego Alves de Souza <diegosouza.mail@gmail.com>
2013-01-08 02:50:20 -0200

    Added in place editting for Todos at index page. Extracted piority_name named options into helper.

--- app/assets/javascripts/todos.js.coffee
+++ app/assets/javascripts/todos.js.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery ->
+  jQuery(".best_in_place").best_in_place()
+  #jQuery('.best_in_place').bind("ajax:success", () -> 
+  #  alert('Updated successfully'))
+  #jQuery('.best_in_place').bind("ajax:error", () -> 
+  #  alert('Could not be updated'))
--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,15 @@
 module TodosHelper
+  def priority_name_options_for(todo)
+    if todo.new_record?
+      (Todo::PRIORITIES - [Todo::DONE]).map {|i| [t("activerecord.attributes.todo.priority_names")[i], i]}
+    else
+      (Todo::PRIORITIES).map {|i| [t("activerecord.attributes.todo.priority_names")[i], i]}
+    end
+  end
+
+  # for some reason the collection expected by best_in_place
+  # has a diferent order than form_for#select
+  def priority_name_options_for_best_in_place
+    (Todo::PRIORITIES).map {|i| [i, t("activerecord.attributes.todo.priority_names")[i]]}
+  end
 end
