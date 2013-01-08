b5dbc167776293ffdeee87758b2423c729a82acd
Diego Alves de Souza <diegosouza.mail@gmail.com>
2013-01-08 01:28:17 -0200

    Added validations to Todo. Mapped priority level to strings in locale file.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,12 @@
 class Todo < ActiveRecord::Base
   attr_accessible :content, :priority, :user_id
+  validates :content, :priotiry, presence: true
+  validates :priotity, numericality:  { integer:  true, greater_than: 0, less_than: 6 }
 
   belongs_to :user
+
+  def priority_name
+    I18n.t("activerecord.attributes.todo.priority_names")[self.priority] 
+  end
+
 end
